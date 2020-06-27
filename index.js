efa8e3e1b44361cf713e90aa82ada1a15ac22350
marcelofag <55140809+marcelofag@users.noreply.github.com>
2020-06-27 15:16:04 -0300

    alteção de teste

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require ("express");
+const bodyParser = require ("body-parser");
+
 const app = express();
+app.use(bodyParser.urlencoded({extended:tu=true}));
 
 app.listen(3000, '127.0.0.1', ()=>{
     console.log('servidor rodando!!!');
@@ -15,8 +18,24 @@ app.get('/pergunta', (req,res)=>{
     res.send('Qual sua pergunta? '+ perg);
 });
 
-app.get('/mensagem/:tipo', (req,res)=>{
-    console.log(req.param);
-    let perg = req.param.tipo;
-    res.send('Qual a opção? '+ perg);
+app.get('/mensagem/:tipo/:id', (req,res)=>{
+    let perg = req.params;
+    res.send('Qual a opção? '+ perg.id);
+});
+
+app.post('/pedido', (req,res)=>{
+    console.log(req.body);
+    const prod = req.body.produto;
+    const quant = req.body.quantidade;
+    const metPagamento = req.body.metodo_pagamento;
+    const bebida = req.body.bebida;
+
+    const pedido = {
+        prod,
+        quant,
+        metPagamento,
+        bebida
+    };
+
+    res.json(pedido);
 });
